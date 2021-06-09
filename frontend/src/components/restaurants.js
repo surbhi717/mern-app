ade77e38beeb0c3f23dcce82e825ad4d3f3e19df
surbhi717 <surbhi7495@gmail.com>
2021-06-09 22:49:32 +0530

    adding frontent components

---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Restaurant() {
+    return (
+        <div className="App">
+            Hello world.
+        </div>
+    );
+}
+
+export default Restaurant;
